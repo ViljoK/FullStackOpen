bc0f2972f3c17cdd4b53b2c96aee2dc0da25e26e
Viljo Karhumaa <viljo.karhumaa@easoft.fi>
2019-06-06 14:47:33 +0300

    Kurssitiedot step1 & gitignore

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+    return (
+        <>
+            <h1>{props.text}</h1>
+        </>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <>
+            <p>{props.part} {props.exercises}</p>
+        </>
+    )
+}
+
+const Total = (props) => {
+    let sum = 0;
+    for (let prop in props) {
+        sum += props[prop];
+    }
+    return (
+        <>
+            <p>Number of exercises {sum}</p>
+        </>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
@@ -12,17 +40,11 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header text={course} />
+            <Content part={part1} exercises={exercises1}/>
+            <Content part={part2} exercises={exercises2}/>
+            <Content part={part3} exercises={exercises3}/>
+            <Total exs1={exercises1} exs2={exercises2} exs3={exercises3}/>
         </div>
     )
 }
