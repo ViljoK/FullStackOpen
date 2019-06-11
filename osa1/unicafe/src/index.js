6ab35d3f6b95dde5bd41ce315d03b601839c95d5
Viljo Karhumaa <viljo.karhumaa@easoft.fi>
2019-06-11 09:01:05 +0300

    step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({good, bad, neutral}) => {
+const Caption = props => <><h1>{props.text}</h1></>
+
+const Statistics = ({good, bad, neutral}) => {
 
     let total = good + bad + neutral;
     let [avg, positive] = [0, 0];
@@ -11,23 +13,22 @@ const Display = ({good, bad, neutral}) => {
     }
 
     return <>
-        <p>{"Good:"} {good}</p>
-        <p>{"Neutral:"} {neutral}</p>
-        <p>{"Bad:"} {bad}</p>
-        <p>{"Total:"} {total}</p>
-        <p>{"Average:"} {avg}</p>
-        <p>{"Positive:"} {positive} {"%"}</p>
+        <Display text={"Good:"} value={good}/>
+        <Display text={"Neutral:"} value={neutral}/>
+        <Display text={"Bad:"} value={bad}/>
+        <Display text={"Total:"} value={total}/>
+        <Display text={"Average:"} value={avg}/>
+        <Display text={"Positive:"} value={positive} unit={"%"}/>
         </>
 }
 
-const Caption = props => <><h1>{props.text}</h1></>
+const Display = props => <><p>{props.text} {props.value}{props.unit}</p></>
 
 const Button = (props) => (
     <button onClick={props.handleClick}>{props.text}</button>
 )
 
 const App = () => {
-    // tallenna napit omaan tilaansa
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
@@ -39,7 +40,7 @@ const App = () => {
             <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"} />
             <Button handleClick={() => setBad(bad + 1)} text={"Bad"} />
             <Caption text="Statistics"/>
-            <Display good={good} bad={bad} neutral={neutral}/>
+            <Statistics good={good} bad={bad} neutral={neutral}/>
         </div>
     )
 }
