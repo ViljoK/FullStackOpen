16d6bf4cef3e5eae1576a46e6a161492f111478c
Viljo Karhumaa <viljo.karhumaa@easoft.fi>
2019-06-11 17:35:21 +0300

    kurssi lisäys

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -3,36 +3,66 @@ import './index.css'
 import Course from './components/Course'
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        },
+        {
+            name: 'React',
+            parts: [
+                {
+                    name: 'App',
+                    exercises: 15,
+                    id: 1
+                },
+                {
+                    name: 'Console',
+                    exercises: 4,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
-        <>
-            <Course course={course} />
-        </>
+        courses.map((course) => <Course key={course.name} course={course}/>)
     )
 }
 
