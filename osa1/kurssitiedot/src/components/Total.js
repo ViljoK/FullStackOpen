51ac7d2a294770aafd2b80ab06c77a2fa46f4e49
Viljo Karhumaa <viljo.karhumaa@easoft.fi>
2019-06-11 15:15:34 +0300

    step8

--- osa1/kurssitiedot/src/components/Total.js
+++ osa1/kurssitiedot/src/components/Total.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const Total = (props) => {
     console.log("Total: ", props)
-    let sum = 0;
-
-    for (let part of props.parts){
-        console.log(part)
-        sum += part.exercises
-    }
+    let sum = props.parts.reduce( (s, p) => {
+        if (s.exercises) {
+            return s.exercises + p.exercises
+        } else {
+            return s + p.exercises
+        }
+    })
     return (
         <>
             <h3>{"Total of " + sum + " exercises"}</h3>
