f719f156b0e0878cfad8bf3bb76528641ee10a3a
Viljo Karhumaa <viljo.karhumaa@easoft.fi>
2019-06-20 14:27:39 +0300

    step3

--- osa2/maatiedot/src/components/Country.js
+++ osa2/maatiedot/src/components/Country.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import List from './List'
 
 const Country = ({country}) => {
     console.log('Country: ', country)
+    const url = 'http://api.apixu.com/v1/current.json?key=bab5fb29f3ed442ebef102431192006&q='
+    const [weather, setWeather] = useState({})
+
+    useEffect(() => {
+        const city = country.capital
+            ? country.capital
+            : 'Paltaniemi'
+        axios
+        .get(url + city)
+        .then(response => {
+            console.log(response.data.location)
+            setWeather(response.data.current)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, [country.capital])
+
+    console.log('WET', weather)
+    const img = weather.condition
+        ? <img src={weather.condition.icon} alt={weather.condition.text}/>
+        : <img alt={'empty'}/>
+    
+    const temp = weather.temp_c !== undefined
+        ? 'Temp: ' + weather.temp_c + ' \xB0C'
+        : 'Not set'
+
+    const wind = weather.wind_kph !== undefined
+        ? 'Wind: ' + weather.wind_kph + ' km/h'
+        : 'Not set'
+
     return (
         <>
         <h1>{country.name}</h1>
@@ -21,6 +53,18 @@ const Country = ({country}) => {
         <h3>Languages</h3>
         <List items={country.languages} ordered={false} />
         <img src={country.flag} alt={country.name} width={150} height={100} />
+        <h3>Weather in {country.capital}</h3>
+        <table>
+            <tbody>
+                <tr>
+                   <td rowSpan={2}>{img}</td>
+                   <td>{temp}</td> 
+                </tr>
+                <tr>
+                    <td>{wind}</td>
+                </tr>
+            </tbody>
+        </table>
         </>
     )
 }
